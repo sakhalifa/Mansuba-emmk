3b325610fc6b6087c64cd03c981d9cccf59cd110
Louis Peyrondet <louis.peyrondet@hotmail.fr>
2022-11-08 18:16:05 +0100

    test neigh functions

---
+++ tst/test.c
@@ -0,0 +1,9 @@
+#include "test.h"
+
+
+int main(){
+    test_get_neighbors();
+    test_get_neighbor();
+
+    return 0;
+}
---
+++ tst/test.h
@@ -0,0 +1,11 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+//*****NEIGHBORS*****//
+bool test_get_neighbors();
+bool test_get_neighbor();
+
+
+
+
+#endif // __TEST_H__
