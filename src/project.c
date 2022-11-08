11feff1f0eb4943ebecf9d2bdf3b5e5167e45105
Louis Peyrondet <louis.peyrondet@hotmail.fr>
2022-11-08 18:24:03 +0100

    added pseudo-code

--- src/project.c
+++ src/project.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "world.h"
+
 
 int main(int argc, char *argv[])
 {
-    printf("hello world\n");
+    struct world_t *w = world_init();
+    // init_neighbors(0);*
+
+    current_player = get_random_player();
+    while nobody has won:
+        p = choose_random_piece_belonging_to(w, current_player);
+        m = choose_random_move_for_piece(w, p);
+        move_piece(w, m);
+        current_player = next_player(current_player);
+
+    
+    return 0;
 }
