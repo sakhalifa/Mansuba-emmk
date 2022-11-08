ec074c68eb8be4993646a2b75e7baf7d45a2b43d
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2022-11-08 16:38:44 +0100

    Added more test and made makefile functional again

--- src/array_list_test.c
+++ src/array_list_test.c
@@ -38,5 +38,21 @@ int main(){
         assert((*(int*)array_list_get(l, i)) == 20);
     }
 
+    //Create a 0-length array_list (will allocate for 1 element)
+    array_list_t *zero_array = array_list_init(0, free_nothing);
+    assert(zero_array->allocated_len == 1);
+    for(int i = 0; i<20; i++){
+        *tab[i] = i;
+        array_list_push(zero_array, tab[i]);
+    }
+
+    for(int i = 0; i<20; i++){
+        assert((*(int*)array_list_get(zero_array, i)) == i);
+    }
+
+
     array_list_free(l);
+    array_list_free(zero_array);
+
+    printf("Successfully tested 'array_list.c'\n");
 }
