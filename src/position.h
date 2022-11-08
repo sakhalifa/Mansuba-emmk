0c602334786a580c9915995ff7ee90a03c7916c0
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2022-11-08 18:29:21 +0100

    Created methods to get all moves from a place in the world

--- src/position.h
+++ src/position.h
@@ -1,9 +1,12 @@
 #ifndef __POSITION_H__
 #define __POSITION_H__
 
-struct position{
+typedef struct position
+{
     int col;
-    int lig;
-};
+    int row;
+} position_t;
+
+int cmp_positions(position_t *pos1, position_t *pos2);
 
 #endif
