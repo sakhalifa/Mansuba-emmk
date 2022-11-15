9bcf3270c53e7e38ebbb11c15ac2f0cb3b12e865
Louis Peyrondet <louis.peyrondet@enseirb-matmeca.fr>
2022-11-15 18:26:41 +0100

    game loop first implementation

---
+++ src/project.h
@@ -0,0 +1,21 @@
+#ifndef __PROJECT_H__
+#define __PROJECT_H__
+
+#include "util.h"
+#include "world.h"
+#include "player.h"
+#include "moves.h"
+#include "geometry.h"
+#include <time.h>
+
+#define MAX_TURN 30
+
+uint choose_random_piece_belonging_to(struct world_t *world, player_t *player);
+node_t *choose_random_move_for_piece(struct world_t *world, uint piece);
+void move_piece(struct world_t *world, node_t *move, player_t *player);
+
+//populates the world as : first row full of black pawn, last row full of white pawn
+void world_populate(struct world_t * world);
+
+
+#endif // __PROJECT_H__
