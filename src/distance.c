343f3dbf523607133d351ed204a5e2f8b69eadd8
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2022-12-06 17:58:19 +0100

    Created basic skeleton for distance.c

--- src/distance.c
+++ src/distance.c
@@ -1,5 +1,33 @@
-#include "util.h" 
-#include "neighbors.h" 
+#include "util.h"
+#include "neighbors.h"
+#include "position.h"
+#include "linked_list.h"
+#include "array_list.h"
 
-unsigned short int distance_lookup_table[MAX_RELATIONS][WORLD_SIZE][WORLD_SIZE];
+unsigned short int *distance_lookup_table = NULL;
 
+void init_distance_lookup_table(array_list_t *starting_pos)
+{
+    if (distance_lookup_table != NULL)
+        free(distance_lookup_table);
+    distance_lookup_table = malloc(sizeof(unsigned short int) * WORLD_SIZE * starting_pos->len * MAX_RELATIONS);    
+}
+
+void compute_distance_lookup_table(array_list_t *starting_pos){
+    
+}
+
+unsigned short int get_distance(position_t *from, uint starting_pos_ind)
+{
+    return *(((distance_lookup_table + get_neighbors_seed() * MAX_RELATIONS * WORLD_SIZE) + position_to_idx(from) * WORLD_SIZE) + starting_pos_ind);
+}
+
+void set_distance(unsigned short int value, position_t *from, uint starting_pos_ind)
+{
+    *(((distance_lookup_table + get_neighbors_seed() * MAX_RELATIONS * WORLD_SIZE) + position_to_idx(from) * WORLD_SIZE) + starting_pos_ind) = value;
+}
+
+void free_distance_lookup_table()
+{
+    free(distance_lookup_table);
+}
