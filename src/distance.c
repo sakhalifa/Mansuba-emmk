eaefa70ae2661a78ac638a5e4eec03b7b04ec5b7
Louis Peyrondet <louis.peyrondet@enseirb-matmeca.fr>
2022-12-06 17:10:00 +0100

    add distance skeleton

---
+++ src/distance.c
@@ -0,0 +1,5 @@
+#include "util.h" 
+#include "neighbors.h" 
+
+unsigned short int distance_lookup_table[MAX_RELATIONS][WORLD_SIZE][WORLD_SIZE];
+
---
+++ src/distance.h
@@ -0,0 +1,8 @@
+#ifndef __DISTANCE_H__
+#define __DISTANCE_H__
+
+
+
+
+#endif // __DISTANCE_H__
+
