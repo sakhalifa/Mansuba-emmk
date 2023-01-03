b7e17a00c5c3d035413b329963d7ab3ffae646ca
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-01-03 15:12:16 +0100

    refactored compute_distance_lookup_table

--- src/distance.c
+++ src/distance.c
@@ -39,6 +39,51 @@ void set_distance(unsigned short int value, relation_t relation, enum color_t co
     *(((distance_lookup_table + relation * WORLD_SIZE * (MAX_COLOR - 1)) + (color - 1) * WORLD_SIZE + index)) = value;
 }
 
+void init_queue(linked_list_t *queue, array_list_t *starting_pos, relation_t relation, enum color_t color)
+{
+    for (uint i = 0; i < starting_pos->len; i++)
+    {
+        game_piece_t *game_piece = array_list_get(starting_pos, i);
+        if (game_piece->piece.color == color)
+        {
+            uint *malloced = malloc(sizeof(uint));
+            *malloced = game_piece->index;
+            linked_list_queue(queue, malloced);
+            set_distance(0, relation, color, game_piece->index);
+        }
+    }
+}
+
+void do_bfs(linked_list_t *queue, relation_t relation, enum color_t color)
+{
+    uint *idx;
+    while ((idx = linked_list_get_last(queue)))
+    {
+        position_t src_pos;
+        position_from_idx(&src_pos, *idx);
+        unsigned short src_dist = get_distance(relation, color, &src_pos);
+        struct neighbors_t neighbors = get_neighbors(*idx);
+
+        for (int i = 0; i < MAX_NEIGHBORS && neighbors.n[i].i != UINT_MAX; i++)
+        {
+            uint cur_neighbor = neighbors.n[i].i;
+            position_t cur_pos;
+            position_from_idx(&cur_pos, cur_neighbor);
+
+            unsigned short dest_dist = get_distance(relation, color, &cur_pos);
+            if (src_dist + 1 < dest_dist)
+            {
+                set_distance(src_dist + 1, relation, color, cur_neighbor);
+                uint *malloced = malloc(sizeof(int));
+                *malloced = cur_neighbor;
+                linked_list_queue(queue, malloced);
+            }
+        }
+
+        linked_list_remove_last(queue);
+    }
+}
+
 void compute_distance_lookup_table(array_list_t *starting_pos, relation_t relation)
 {
     relation_t old_rel = get_neighbors_seed();
@@ -46,43 +91,8 @@ void compute_distance_lookup_table(array_list_t *starting_pos, relation_t relati
     for (enum color_t color = NO_COLOR + 1; color < MAX_COLOR; color++)
     {
         linked_list_t *queue = linked_list_create(free);
-        for (uint i = 0; i < starting_pos->len; i++)
-        {
-            game_piece_t *game_piece = array_list_get(starting_pos, i);
-            if (game_piece->piece.color == color)
-            {
-                uint *malloced = malloc(sizeof(int));
-                *malloced = game_piece->index;
-                linked_list_queue(queue, malloced);
-                set_distance(0, relation, color, game_piece->index);
-            }
-        }
-        uint *idx;
-        while ((idx = linked_list_get_last(queue)))
-        {
-            position_t src_pos;
-            position_from_idx(&src_pos, *idx);
-            unsigned short src_dist = get_distance(relation, color, &src_pos);
-            struct neighbors_t neighbors = get_neighbors(*idx);
-
-            for (int i = 0; i < MAX_NEIGHBORS && neighbors.n[i].i != UINT_MAX; i++)
-            {
-                uint cur_neighbor = neighbors.n[i].i;
-                position_t cur_pos;
-                position_from_idx(&cur_pos, cur_neighbor);
-
-                unsigned short dest_dist = get_distance(relation, color, &cur_pos);
-                if (src_dist + 1 < dest_dist)
-                {
-                    set_distance(src_dist + 1, relation, color, cur_neighbor);
-                    uint *malloced = malloc(sizeof(int));
-                    *malloced = cur_neighbor;
-                    linked_list_queue(queue, malloced);
-                }
-            }
-
-            linked_list_remove_last(queue);
-        }
+        init_queue(queue, starting_pos, relation, color);
+        do_bfs(queue, relation, color);
         linked_list_free(queue);
     }
     init_neighbors(old_rel);
