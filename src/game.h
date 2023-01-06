68793cc8bbc0f9f0fdd420393de1b55bb5ed1153
Louis Peyrondet <louis.peyrondet@enseirb-matmeca.fr>
2023-01-06 14:33:24 +0100

    some game documention

--- src/game.h
+++ src/game.h
@@ -45,6 +45,9 @@ void current_player_move_piece(game_t *game, const node_t *move);
 //return if the escape was successful or not
 bool current_player_try_escape(game_t *game, game_piece_t piece);
 
+/// @brief Checks if the current game is in a win  
+/// @param game the game structure
+/// @return true if the game is won, false otherwise
 bool check_win(const game_t *game);
 
 
@@ -52,6 +55,9 @@ bool check_win(const game_t *game);
 /// @param game pointer on the game stucture
 void display_game(const game_t *game);
 
+
+/// @brief Frees the game structure and all it's allocated memory
+/// @param game the game structure
 void game_free(game_t *game);
 
 void capture_piece_at(game_t *game, uint index);
@@ -65,5 +71,10 @@ void world_populate(const game_t *game);
 /// @param game the game structure  
 void load_starting_position(game_t *game);
 
+
+/// @brief Checks if the given player has at least one piece captured 
+/// @param game the game structure
+/// @param player the player to check 
+/// @return true if the player has a piece captured, false otherwise 
 bool has_piece_captured(const game_t *game, const player_t *player);
 #endif // __GAME_H__
